c18f9cb37688993ed9b000493f836746f92ad81b
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-07-26 14:49:01 +0700

    feat(web): setup client side using CRA

--- server/app.js
+++ server/app.js
@@ -23,6 +23,6 @@ app.use(
     })
 );
 
-app.listen(3000, () => {
-    console.log("now listening for requests on port 3000");
+app.listen(4000, () => {
+    console.log("now listening for requests on port 4000");
 });
